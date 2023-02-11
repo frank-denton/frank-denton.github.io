b315457f4882468c0f3e5e106af18fe85638b325
Unabatedlynx5 <57015501+Unabatedlynx5@users.noreply.github.com>
2023-02-11 12:08:03 -0600

    changed response to xl

--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -16,7 +16,7 @@ function About() {
         <h1>Welcome!</h1><br></br>
         <h2>This website was created by: Frank Denton</h2><br></br><br></br>
         <Row className='d-flex align-items-center'>
-            <Col m={6}>
+            <Col xl={6}>
                 <Image width="100%" hight="100%" src={profile} roundedCircle></Image>
             </Col>
             <Col xs={5} className='align-middle'>
