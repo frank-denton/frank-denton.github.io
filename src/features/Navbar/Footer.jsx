848181c571d7e595887fadbf0a33e5b598c0f837
Unabatedlynx5 <frankrobertdenton@gmail.com>
2023-02-11 23:23:27 -0600

    Changed about and footer links

--- src/features/Navbar/Footer.jsx
+++ src/features/Navbar/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => <footer className="page-footer font-small blue pt-4 bg-prim
     <div className="container-fluid text-center text-md-left">
         <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
-                <h5 className="text-uppercase">Footer Content</h5>
-                <p>Here you can use rows and columns to organize your footer content.</p>
+                <h5 className="text-uppercase">Contact Me</h5>
+                <p>Learn more about me and what I am working on.</p>
             </div>
 
             {/* <hr className="clearfix w-100 d-md-none pb-0"/> */}
@@ -15,26 +15,26 @@ const Footer = () => <footer className="page-footer font-small blue pt-4 bg-prim
             <div className="col-md-3 mb-md-0 mb-3">
                 <h5 className="text-uppercase">Links</h5>
                 <ul className="list-unstyled">
-                    <li><Button variant='link'>LinkedIn</Button></li>
-                    <li><Button variant='link'>Instagram</Button></li>
-                    <li><Button variant='link'>Twitter</Button></li>
-                    <li><Button variant='link'>Github</Button></li>
+                    <li><Button variant='link' href='https://www.linkedin.com/in/frank-denton-423b4120a/' target="_blank" rel="noopener noreferrer">LinkedIn</Button></li>
+                    {/* <li><Button variant='link'>Instagram</Button></li> */}
+                    {/* <li><Button variant='link'>Twitter</Button></li> */}
+                    <li><Button variant='link' href='https://github.com/Unabatedlynx5' target="_blank" rel="noopener noreferrer">Github</Button></li>
                 </ul>
             </div>
             <div className="col-md-3 mb-md-0 mb-3">
                 <h5 className="text-uppercase">Links</h5>
                 <ul className="list-unstyled">
                     <li><Button variant='link' onClick={() => window.location = 'mailto:frankrobertdenton@gmail.com'}>Email</Button></li>
-                    <li><Button variant='link'>School</Button></li>
-                    <li><Button variant='link'>Podcast</Button></li>
-                    <LinkContainer to='/234'><li><Button variant='link'>Fun!</Button></li></LinkContainer>
+                    <li><Button variant='link' href='https://www.totinograce.org/' target="_blank" rel="noopener noreferrer">School</Button></li>
+                    {/* <li><Button variant='link'>Podcast</Button></li> */}
+                    {/* <LinkContainer to='/234'><li><Button variant='link'>Fun!</Button></li></LinkContainer> */}
                 </ul>
             </div>
         </div>
     </div>
 
     <div className="footer-copyright text-center py-3">© 2023 Copyright: {' '}
-        <a href="https://mdbootstrap.com/" className="text-info">MDBootstrap.com</a>
+    <a className="text-info">Frank Denton</a>
     </div>
 
 </footer>
--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -22,7 +22,7 @@ function About() {
             <Col md className='align-middle'>
                 <h4>About me:</h4>
                 <br></br>
-                <p className='text-info'>I am currently attending Totino-Grace High School as a Senior. During my time here, I have been involved in great college-prep classes and opportunities. I have been in First Robotics Competition for 4 years, where I have learned how to program and create solutions to unique challenges every year. This year I am working on computer vision for the robot. During Robotics, I was able to meet some amazing sponsors at Delkor and Minnetronix. I also attended the E3 institute where I learned the design process, and how to think critically and execute solutions. I was also able to get real-world experience with a mentor. I was able to shadow him at work and learn about the complexity and management it takes to design the products we use every day.  I am currently a part of S.O.A.R., a leadership group where I have learned how to be a servant leader and how to focus on my mentality to help our team. Outside of School, I am working at Dairy Queen where I am able to learn how to work in a fast-paced environment with others.</p>
+                <p className='text-info'>I am currently attending Totino-Grace High School as a Senior. During my time here, I have been involved in great college-prep classes and opportunities. I have been in First Robotics Competition for 4 years, where I have learned how to program and create solutions to unique challenges every year. This year I am working on computer vision for the robot. During Robotics, I was able to meet some amazing sponsors at Delkor and Minnetronix. I also attended the E3 institute where I learned the design process, and how to think critically and execute solutions. I was also able to get real-world experience with a mentor. I was able to shadow him at work and learn about the complexity and management it takes to design the products we use every day. Outside of class, I play soccer in the fall, swim in the winter, and run track in the spring. This year I was elected captain of my swim team and my coach recommended me for the S.O.A.R. leadership group at Totino-Grace. I also participate in Trapshooting and Mountain Biking with my Dad and the School. Outside of School, I am working at Dairy Queen where I am able to learn how to work in a fast-paced environment with others. Every summer since 8th grade I have gone to Extreme Faith Camp (EFC) or Catholic Youth Summer Camp (CYSC). I have been able to build and deepen my relationship through the camp, and in the last three years, I have been able to help middle schoolers deepen their relationship. During High School, I have been able to create a well-rounded base of skills that will only continue to grow in the future. I hope to pursue a Computer Engineering Degree, and use my talents to create technology that helps to create a brighter future.</p>
             </Col>
         </Row>
 
@@ -31,17 +31,18 @@ function About() {
         
         <div className='about text-info'>
             <h4 className='text-light'>Education:</h4>
-            <h5 >St. John the Baptist Catholic School</h5>
+            <h5><a href='https://stjohnnyb.org/' className='text-info' target="_blank" rel="noopener noreferrer">St. John the Baptist Catholic School</a></h5>
             <p >2008-2019</p>
             <br></br>
-            <h5>Totino-Grace High-School</h5>
+            <h5><a href='https://www.totinograce.org/' className='text-info' target="_blank" rel="noopener noreferrer">Totino-Grace High-School</a></h5>
             <p >2019-2023</p>
             <br></br>
             <h4 className='text-light'>Skills:</h4>
-            <h5 >Leadership:</h5>
+            <h5 ><a href='https://www.totinograce.org/activities/clubs' className='text-info' target="_blank" rel="noopener noreferrer">Leadership:</a></h5>
             <p>S.O.A.R. leadership group at Totino-Grace.</p>
+            <p>Swim Team Captain</p>
             <br></br>
-            <h5 >Engineering:</h5>
+            <h5 ><a href='https://www.totinograce.org/academics/engineering-institute' className='text-info'  target="_blank" rel="noopener noreferrer">Engineering:</a></h5>
             <p>Experimental Engineering Education (E3) program.</p>
             <br></br>
             <h5 >JavaScript:</h5>
